1942dd6d0a2ba4fa90edfd7096e39ac402fafcbf
whydidIchoosephysics <ionelvlad123@gmail.com>
2023-03-02 20:29:21 +0000

    Initial files created with base class employee

---
+++ assets/Javascript/manager.js
@@ -0,0 +1,16 @@
+import Employee from "./employee";
+
+class Manager extends Employee {
+  constructor(name, id, email, officeNumber) {
+    this.officeNumber = officeNumber;
+
+    super(name, id, email);
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
+
+  getRole() {
+    return "Manager";
+  }
+}
